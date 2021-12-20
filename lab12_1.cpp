bb8f7550ebf12fad9d46302a3b5a871bd8b130e7
Phanthat Muenprap <phanthat54@gmail.com>
2021-12-20 23:43:07 +0700

    lab12_1 finish

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,20 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main(){
+    string randGrade[] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+    cout << "Press Enter 3 times to reveal your future.";
+
+    cin.get();
+    cin.get();
+    cin.get();
+
+    srand(time(0));
+    int i = rand() % 9;
+    
+    cout << "You will get "<<randGrade[i] <<" in this 261102.\n";
+    return 0;
+}
